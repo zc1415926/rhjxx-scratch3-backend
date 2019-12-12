c23defed09b7a0ee3774e26dfd273c9e0e8c6c08
ZhangChuan <zc1415926@126.com>
2019-12-12 11:42:38 +0800

    提升文件大小限制

--- app.js
+++ app.js
@@ -3,16 +3,10 @@ let bodyParser = require('body-parser');
 let fs = require('fs');
 
 var app = express()
-    .use(bodyParser.json());
+    //设置文件大小限制：https://blog.csdn.net/qq_36060786/article/details/85238836
+    .use(bodyParser.json({limit : "100mb"}));
 
-// 中间件，应用的每个请求都会执行该中间件
-/* app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Methods' , 'POST, GET');
-    res.header('Access-Control-Allow-Credentials' , 'ture');
-    next();
-}) */
-
-//解决Scratch3提交时的跨域错误
+//用中间件解决Scratch3提交时的跨域错误
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -27,7 +21,7 @@ app.post('/scratch-file', (req, res)=>{
     //这里的req.body需要body-parser中间件，req.body.fileData是sb3文件的实际base64数据。
     // console.log(req.body.fileData)
 
-    //把base64数据写入文件，https://cloud.tencent.com/developer/ask/61089
+    //把base64数据写入文件：https://cloud.tencent.com/developer/ask/61089
     var base64Data = req.body.fileData.replace("data:application/x.scratch.sb3;base64,", "");
     var binaryData = new Buffer(base64Data, 'base64').toString('binary');
     fs.writeFile(req.body.fileName, binaryData, "binary", function(err) {

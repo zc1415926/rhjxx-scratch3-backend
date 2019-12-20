896237dfb2dcf91939ae869a2d3c055676d117fc
ZhangChuan <zc1415926@126.com>
2019-12-20 11:21:43 +0800

    根据fileInfo在文件夹中保存sb3文件
    使用fs-extra提升写入文件的便捷性

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let fs = require('fs');
+let fs = require('fs-extra');
 
 var app = express()
     //设置文件大小限制：https://blog.csdn.net/qq_36060786/article/details/85238836
@@ -19,18 +19,25 @@ app.get('/', (req, res)=>{
 
 app.post('/scratch-file', (req, res)=>{
     //这里的req.body需要body-parser中间件，req.body.fileData是sb3文件的实际base64数据。
-    // console.log(req.body.fileData)
 
     //把base64数据写入文件：https://cloud.tencent.com/developer/ask/61089
     var base64Data = req.body.fileData.replace("data:application/x.scratch.sb3;base64,", "");
     var binaryData = new Buffer.from(base64Data, 'base64').toString('binary');
-    fs.writeFile(req.body.fileName, binaryData, "binary", function(err) {
-        if(err){
-            throw err;
-        }
 
-        //TODO:写入文件后测试写入是否成功
-    });
+    let fileFullPath = 'sb3Files/';
+    fileFullPath += req.body.fileInfo[0] + '/' //年级id
+                    + req.body.fileInfo[1] + '/' //课题id
+                    + req.body.fileInfo[2] + '/' //班级id
+                    + req.body.fileInfo[3] + '.sb3' //学生id.sb3
+    /* 没有使用req.body.fileName（Scratch3文件名框中输入的文件名）因为可以是汉字等不易于保存的字符
+    后期可以用于向数据库中保存文件信息。 */
+
+    fs.outputFile(fileFullPath, binaryData, 'binary', err=>{
+        if(err){
+            console.log(err);
+        }
+        console.log('TODO:写入文件后测试写入是否成功，并向前端返回结果');
+    })
 
     res.status(200).send('done');
 })
